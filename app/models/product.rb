312a03da4eb15a38234e7db80d8fc2619c8c6874
caspg <kacper.golinski@gmail.com>
2015-02-21 13:58:56 +0100

    validates product's name

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   has_many :servings
   has_many :recipe_products
   has_many :recipes, through: :recipe_products
+
+  validates :name, presence: true, format: { with: /\A[\w\-\s]+\z/, message: 'only allows letters!' }
 end
