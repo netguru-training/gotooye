0e570426f0ffdca2794c7a8ee0d8e6a41d3733ca
Natalia Anna Tyszkiewicz <tyszkiewicz@Natalias-MacBook-Air.local>
2015-02-21 11:26:27 +0100

    minor clean up

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221092342) do
+ActiveRecord::Schema.define(version: 20150221094847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
-    t.string   "CreateProducts"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
