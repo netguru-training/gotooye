fd30c6e318e1840b1e6536fb787bbe4f92fce0bb
caspg <kacper.golinski@gmail.com>
2015-02-21 11:16:41 +0100

    add create method to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,19 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def create
+    self.product = Product.new(product_params)
+    if product.save
+      redirect_to products_path, flash: { notice: 'Product successfully added!' }
+    else
+      render action: 'new'
+    end
+  end
+
+  private 
+
+    def product_params
+      params.require(:product).permit(:name)
+    end
+
 end
