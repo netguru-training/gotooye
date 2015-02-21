548c4d3bd18e977f4f8ba14434661085d33fdc9e
Natalia Anna Tyszkiewicz <tyszkiewicz@Natalias-MacBook-Air.local>
2015-02-21 10:51:51 +0100

    create a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,19 @@ class RecipesController < ApplicationController
   expose(:recipes)
   expose(:recipe)
 
-  def index
+  def create
+    self.recipe = Recipe.new(recipe_params)
+    if recipe.save
+      redirect_to recipe, notice: 'Recipe was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :desc)
   end
 
 end
