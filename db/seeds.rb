6ae74f289d601d62ee589eb029318ac473d2bf11
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-21 13:35:43 +0100

    Improve user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#user = CreateAdminService.new.call
-#puts 'CREATED ADMIN USER: ' << user.email
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email
 
 User.create(
   name: "Example User",
@@ -15,13 +15,13 @@ User.create(
   password_confirmation: "12345678"
 )
 
-# users = []
-# 10.times do 
-#   users << User.create(
-#     name: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password: "12345678",
-#     password_confirmation: "12345678"
-#   )
-# end
+users = []
+10.times do 
+  users << User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "12345678",
+    password_confirmation: "12345678"
+  )
+end
 
