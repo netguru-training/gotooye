e01de43d9b0dfee56d58af1454644114b966a2ac
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-21 11:47:37 +0100

    Add seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+User.create(
+  name: "Example User",
+  email: "example@mail.com",
+  password: "12345678",
+  password_confirmation: "12345678"
+)
+
+users = []
+10.times do 
+  users << User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "12345678",
+    password_confirmation: "12345678"
+  )
+end
+
