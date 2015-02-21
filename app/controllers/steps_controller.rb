3b2e660a5eb71f771c121c7fd939abe74d85866a
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-21 16:59:13 +0100

    Allow steps to be saved

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,9 +1,25 @@
 class StepsController < ApplicationController
 
   expose :recipe
-  expose :step
+  expose(:step, attributes: :step_params)
 
   def new
     
   end
+
+  def create
+    if step.save!
+      flash[:notice] = "step created"
+      redirect_to recipe
+    else
+      flash[:alert] = "step not saved"
+      render 'new'
+    end
+  end
+
+  private
+
+    def step_params
+      params.require(:step).permit(:description)
+    end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,5 @@
 class Step < ActiveRecord::Base
   belongs_to :recipe
+
+  validates :description, presence: true
 end
