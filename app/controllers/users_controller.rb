e0552f81fc57c4fbd5519ef5ce0881e0c979079b
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-21 15:55:08 +0100

    Add users index and user show, add users link to nav

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  expose :user
+  expose :users
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :recipes
   devise_for :users
 
+  get '/users/', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  
   root to: 'visitors#index'
 
   resources :products, only: [:index, :new, :create, :edit, :update, :destroy] do
