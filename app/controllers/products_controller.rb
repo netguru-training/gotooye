34512931d9692a8fa886f0e8c200d8c78ff80dcc
caspg <kacper.golinski@gmail.com>
2015-02-22 15:13:47 +0100

    fix products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   expose(:products)
   expose(:product, attributes: :product_params)
