54a69b83ea28599f023098aea65428e017cb36b1
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-22 12:21:27 +0100

    Put recipe's steps into accordon

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -3,4 +3,18 @@ module RecipeHelper
   def ingredients
     Product.by_name
   end
+
+  def step_expanded?(index)
+    result = {
+      expanded: "false",
+      in: ""
+    }
+
+    if index == 0
+      result[:expanded] = "true"
+      result[:in] = "in"
+    end
+    result
+  end
+
 end
