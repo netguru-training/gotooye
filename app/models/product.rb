729e9b8b3b5555e0201ee70573d434fafecd4fd4
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-22 10:25:11 +0100

    Make product name to be unique

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,11 @@ class Product < ActiveRecord::Base
   has_many :recipes, through: :recipe_products
   has_many :favourite_products
 
-  validates :name, presence: true, format: { with: /\A[\w\-\s]+\z/, message: 'only allows letters!' }
+  validates :name, presence: true, 
+                   uniqueness: true, 
+                   format: { 
+                      with: /\A[\w\-\s]+\z/, 
+                      message: 'only allows letters!' 
+                   }
   scope :by_name, -> { order('name') }
 end
