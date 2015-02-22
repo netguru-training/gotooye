144482c39252a387ad37d0a00b470476bc752c18
Dominik Wojciechowski <dominikk.wojciechowski@gmail.com>
2015-02-22 10:41:00 +0100

    Add picture to step

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,4 +1,7 @@
 class Step < ActiveRecord::Base
+  has_attached_file :picture, styles: { medium: "200x200>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
   belongs_to :recipe
 
   validates :description, presence: true
