1f411c60427eb7fa58d0344a8b5aafcb436ff2c0
Natalia Anna Tyszkiewicz <tyszkiewicz@Natalias-MacBook-Air.local>
2015-02-22 14:52:32 +0100

    dem conflictz

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150222134628) do
+ActiveRecord::Schema.define(version: 20150222134831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
