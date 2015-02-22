5754d815be50a575cac8d76d67ae12724192f3f0
Natalia Anna Tyszkiewicz <tyszkiewicz@Natalias-MacBook-Air.local>
2015-02-22 09:55:53 +0100

    add relation between recipe_product and servings

--- app/models/recipe_product.rb
+++ app/models/recipe_product.rb
@@ -1,5 +1,6 @@
 class RecipeProduct < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :product
+  belongs_to :serving
 
 end
--- app/models/serving.rb
+++ app/models/serving.rb
@@ -1,5 +1,6 @@
 class Serving < ActiveRecord::Base
   belongs_to :product
+  has_many :recipe_products
 
   validates :name, presence: true, format: { with: /\A[\w\-\s]+\z/, message: 'only allows letters!' }
 
